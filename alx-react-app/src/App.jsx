6109830be8e1b2a6971d220ab5c316619d3cf035
Nana Safo <iamnanasafo@gmail.com>
2025-08-30 13:16:34 +0000

    created a new react vite app

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
+import MainContent from './MainContent';
+import Footer from './Footer';
+import Header from './Header';
+import UserProfile from './components/UserProfiles'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+
+      <h1>User Profiles</h1>
+      <UserProfile
+        name = "Nana Kwesi"
+        age = "42"
+        bio = "Loves to code in Python"/>
+      
+      <UserProfile
+        name = "Ama Tina"
+        age = "19"
+        bio = "Always in the kitchn cooking the best meal you've ever tasted"/>
+      
+
+    </>
+  )
+}
+
+export default App
